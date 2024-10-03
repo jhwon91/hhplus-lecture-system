55d6dd05a3e150ded5a5317a2c70c94b6e281910
won <jwon3991.9191@gmail.com>
2024-10-04 05:40:40 +0900

    test: UserServiceTest 단위 테스트 수정

--- src/test/java/com/hhplus/clean/domain/UserServiceTest.java
+++ src/test/java/com/hhplus/clean/domain/UserServiceTest.java
@@ -1,8 +1,11 @@
 package com.hhplus.clean.domain;
 
+import com.hhplus.clean.domain.dto.UserCommand;
+import com.hhplus.clean.domain.dto.UserInfo;
 import com.hhplus.clean.domain.entity.UserEntity;
 import com.hhplus.clean.domain.Repository.UserRepository;
 import com.hhplus.clean.domain.service.UserService;
+import org.aspectj.bridge.ICommand;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -14,105 +17,94 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 public class UserServiceTest {
-//
-//    @Mock
-//    private UserRepository userRepository;
-//
-//    @InjectMocks
-//    private UserService userService;
-//
-//    @BeforeEach
-//    void setUp(){
-//        MockitoAnnotations.openMocks(this);
-//    }
-//
-//    //given
-//    //when
-//    //then
-//    @Test
-//    void 사용자_생성(){
-//        //given
-//        UserEntity user = new UserEntity();
-//        user.setUsername("testUser");
-//
-//        UserEntity saveUser = new UserEntity();
-//        saveUser.setId(1L);
-//        saveUser.setUsername("testUser");
-//
-//        when(userRepository.save(user)).thenReturn(saveUser);
-//
-//        //when
-//        UserEntity result = userService.saveUser(user);
-//
-//        //then
-//        assertEquals(1L, result.getId());
-//        assertEquals("testUser", result.getUsername());
-//    }
-//
-//    @Test
-//    void 단일_사용자_조회() {
-//        //given
-//        long userId = 1L;
-//        UserEntity user_1 = new UserEntity();
-//        user_1.setId(userId);
-//        user_1.setUsername("testUser");
-//
-//        when(userRepository.findById(userId)).thenReturn(Optional.of(user_1));
-//
-//        //when
-//        UserEntity result = userService.getUser(userId);
-//
-//        //then
-//        assertEquals(userId, result.getId());
-//        assertEquals("testUser", result.getUsername());
-//    }
-//
-//    @Test
-//    void 사용자_목록_조회() {
-//        //given
-//        UserEntity user_1 = new UserEntity();
-//        user_1.setId(1L);
-//        user_1.setUsername("testUser");
-//
-//        UserEntity user_2 = new UserEntity();
-//        user_2.setId(2L);
-//        user_2.setUsername("testUser2");
-//
-//        UserEntity user_3 = new UserEntity();
-//        user_3.setId(3L);
-//        user_3.setUsername("testUser3");
-//
-//        when(userRepository.findAll()).thenReturn(List.of(user_1,user_2,user_3));
-//
-//        //when
-//        List<UserEntity> result = userService.getAllUsers();
-//
-//        //then
-//        assertEquals(3, result.size());
-//        assertEquals(user_1, result.get(0));
-//        assertEquals(user_2, result.get(1));
-//        assertEquals(user_3, result.get(2));
-//    }
-//
-//    @Test
-//    void 사용자가_존재하지_않을때_예외() {
-//        //given
-//        long userId = 1L;
-//
-//        when(userRepository.findById(userId)).thenReturn(Optional.empty());
-//
-//        //when
-//        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-//            userService.getUser(userId);
-//        });
-//
-//        //then
-//        assertEquals("해당 유저를 찾을 수 없습니다.", exception.getMessage());
-//    }
-//
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+    @Test
+    void 사용자_생성(){
+        //given
+        UserCommand user = new UserCommand(1L,"testUser");
+
+        UserEntity saveUser = new UserEntity();
+        saveUser.setId(1L);
+        saveUser.setUsername("testUser");
+
+        when(userRepository.save(any(UserEntity.class))).thenReturn(saveUser);
+
+        //when
+        UserInfo result = userService.saveUser(user);
+
+        //then
+        assertEquals(1L, result.id());
+        assertEquals("testUser", result.userName());
+    }
+
+    @Test
+    void 단일_사용자_조회() {
+        //given
+        long userId = 1L;
+        UserCommand user_1 = new UserCommand(userId,"testUser");
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user_1.toEntity()));
+
+        //when
+        UserInfo result = userService.getUser(userId);
+
+        //then
+        assertEquals(userId, result.id());
+        assertEquals("testUser", result.userName());
+    }
+
+    @Test
+    void 사용자_목록_조회() {
+        //given
+        UserCommand user_1 = new UserCommand(1L,"testUser");
+        UserCommand user_2 = new UserCommand(2L,"testUser2");
+        UserCommand user_3 = new UserCommand(3L,"testUser3");
+
+
+        when(userRepository.findAll()).thenReturn(List.of(user_1.toEntity(),user_2.toEntity(),user_3.toEntity()));
+
+        //when
+        List<UserInfo> result = userService.getAllUsers();
+
+        //then
+        assertEquals(3, result.size());
+        assertEquals(user_1.id(), result.get(0).id());
+        assertEquals(user_2.id(), result.get(1).id());
+        assertEquals(user_3.id(), result.get(2).id());
+    }
+
+    @Test
+    void 사용자가_존재하지_않을때_예외() {
+        //given
+        long userId = 1L;
+
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        //when
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            userService.getUser(userId);
+        });
+
+        //then
+        assertEquals("해당 유저를 찾을 수 없습니다.", exception.getMessage());
+    }
+
 
 
 }
