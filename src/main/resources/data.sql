8111b912b2ea08b928154afb6c4db61dddcc5352
won <jwon3991.9191@gmail.com>
2024-10-04 04:48:57 +0900

    properties 수정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+
+insert into users ( user_name) values ('test');
+insert into users ( user_name) values ('test1');
+insert into users ( user_name) values ('test2');
+
+insert into lecture (lecture_name) values ('test');
+insert into lecture (lecture_name) values ('test1');
+insert into lecture (lecture_name) values ('test2');
+
+insert into lecture_schedule (lecture_id, reg_date, capacity, current_count) values (1,'2024-10-01',30,0);
+insert into lecture_schedule (lecture_id, reg_date, capacity, current_count) values (1,'2024-10-02',30,0);
+insert into lecture_schedule (lecture_id, reg_date, capacity, current_count) values (1,'2024-10-03',30,0);
+insert into lecture_schedule (lecture_id, reg_date, capacity, current_count) values (2,'2024-10-02',30,0);
+insert into lecture_schedule (lecture_id, reg_date, capacity, current_count) values (2,'2024-10-03',30,0);
+insert into lecture_schedule (lecture_id, reg_date, capacity, current_count) values (3,'2024-10-01',30,0);
+insert into lecture_schedule (lecture_id, reg_date, capacity, current_count) values (3,'2024-10-02',30,0);
+insert into lecture_schedule (lecture_id, reg_date, capacity, current_count) values (3,'2024-10-03',30,0);
